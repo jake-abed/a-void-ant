6e72d386a5c3f2471a3534c1947364432153d8a4
Jake Abed <jakeabed@gmail.com>
2024-05-16 23:08:44 -0700

    Add hills 2, dash ant, slime movement

--- assets/scripts/scene_manager.gd
+++ assets/scripts/scene_manager.gd
@@ -3,6 +3,7 @@ extends Node
 const scene_void = preload("res://scenes/levels/void.tscn")
 const scene_hills = preload("res://scenes/levels/hills.tscn")
 const scene_cave = preload("res://scenes/levels/cave.tscn")
+const scene_hills2 = preload("res://scenes/levels/hills_2.tscn")
 
 signal room_change
 
@@ -21,6 +22,8 @@ func go_to_level(level: String, destination: String) -> void:
 			scene_to_load = scene_hills
 		"Cave":
 			scene_to_load = scene_cave
+		"Hills2":
+			scene_to_load = scene_hills2
 		
 	if scene_to_load != null:
 		spawn_door_tag = destination
--- assets/scripts/slime.gd
+++ assets/scripts/slime.gd
@@ -3,9 +3,12 @@ extends CharacterBody2D
 @export var sprite: AnimatedSprite2D
 @export var hitbox: Area2D
 @export var hurtbox: Area2D
+@export var ray: RayCast2D
 
-const SPEED = 300.0
+const SPEED = 50.0
 const JUMP_VELOCITY = -400.0
+
+@onready var facing_factor := 1.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -19,9 +22,18 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	move_x()
+	
 	move_and_slide()
 
 func _hitbox_entered(body: Node2D) -> void:
 	if body is Player:
 		body.take_damage(1)
+
+func move_x() -> void:
+	if (!ray.is_colliding() or is_on_wall()) and is_on_floor():
+		ray.position.x *= -1.0
+		ray.target_position.x *= -1.0
+		sprite.flip_h = !sprite.flip_h
+		facing_factor *= -1.0
+	velocity.x = SPEED * facing_factor
